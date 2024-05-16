1687ad11cad02495ffe32bd286053a8cc58f7efa
theapache64 <theapache64@gmail.com>
2024-05-16 12:37:57 +0530

    🆙 update image

--- app/src/main/java/io/github/theapache64/background_thread_poc/App.kt
+++ app/src/main/java/io/github/theapache64/background_thread_poc/App.kt
@@ -1,12 +1,17 @@
 package io.github.theapache64.background_thread_poc
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
 class App : Application() {
+    private val backgroundScope = CoroutineScope(Dispatchers.Default)
+
     override fun onCreate() {
         super.onCreate()
         (applicationContext as Application).registerActivityLifecycleCallbacks(
             AppStartUpLifeCycleCallbacks()
         )
+
     }
 }
